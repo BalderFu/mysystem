30a63ecf2729d445e225dfe27de597407c232ac9
Jin <jin@mail.com>
2025-03-25 06:12:35 +0800

    表结构初始化

---
+++ sql/init.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `jz_sensitive`.`yonghu` (`id`, `role`, `username`, `password`, `phone`, `avatar`, `email`) VALUES
+(1, 'admin', 'admin', '123456', '13412345678', 'default.jpg', '123456789@qq.com');
+
