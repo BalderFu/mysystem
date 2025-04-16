cac4950d8edf3216af431e4b82c1f1f4304c77b4
Jin <jin@mail.com>
2025-04-16 19:14:17 +0800

    tag2

--- backend/sql/init.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO `my_system`.`user` ( `role`, `username`, `password`, `phone`, `email`) VALUES
-('admin', 'admin', '123456', '13412345678',  '123456789@qq.com');
-
-
--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -7,11 +7,13 @@ CREATE TABLE `my_system`.`user`
     `username`    varchar(255) NOT NULL COMMENT '用户名',
     `password`    varchar(255) NOT NULL COMMENT '密码',
     `phone`    varchar(255) NOT NULL COMMENT '手机',
-   -- `avatar`    varchar(255) NOT NULL COMMENT '头像',
+    `nickname`    varchar(255) NOT NULL COMMENT '昵称',
     `email`    varchar(255) NOT NULL COMMENT '邮箱',
     `create_time` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `update_time` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE = InnoDB COMMENT = '用户表';
 
+INSERT INTO `my_system`.`user` ( `role`, `username`, `password`, `phone`, `email`,`nickname`) VALUES
+('admin', 'admin', '123456', '13412345678',  '123456789@qq.com','管理员');
 
 
